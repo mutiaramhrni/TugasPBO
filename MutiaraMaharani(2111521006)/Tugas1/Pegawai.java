4745a2ebf55213e521e19b1c920856d4cbe56c6c
Mutiara Maharani <89976205+mutiaramhrni@users.noreply.github.com>
2022-11-06 11:52:31 +0700

    Add files via upload

---
+++ MutiaraMaharani(2111521006)/Tugas1/Pegawai.java
@@ -0,0 +1,42 @@
+public class Pegawai {
+    private String name;
+    private String address;
+    private int number;
+
+    public Pegawai(String name, String address, int number) {
+        /*
+         * System.out.println("Menyusun Pegawai "); --> karena ada string yg dibuat
+         * pada constructor superclass,maka akan menjadi referensi bagi subclass yaitu
+         * class gaji.Maka ini dinamakan
+         * dengan DOWNCASTING
+         */
+        this.name = name;
+        this.address = address;
+        this.number = number;
+
+    }
+
+    public void mailCheck() {
+        System.out.println("Memeriksa surat untuk " + this.name + "" + this.address);
+    }
+
+    public String toString() {
+        return name + "" + address + "" + number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String newAddress) {
+        address = newAddress;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+}
